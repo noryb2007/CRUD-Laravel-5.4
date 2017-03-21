e3bf9a1fe60707b2c1642ecf0d410baea5b68b1f
NORYB2007 <bygishalom@gmail.com>
2017-03-20 20:21:39 -0500

    Creacion modelo y controlador

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    
+    return [
+        'name'  => $faker->sentence(4),
+        'short' => $faker->sentence(12),
+        'body'  => $faker->text(1200),
+        
+    ];
+});
